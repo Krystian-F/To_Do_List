253005b707c5abbc57ed0c45a9b20721e1f61e26
Krystian-F <krystianfpa@gmail.com>
2024-12-24 23:58:36 +0100

    display_tasks- sorting and display of tasks

--- src/display_tasks.js
+++ src/display_tasks.js
@@ -1,14 +1,33 @@
 let displayTasks = function () {
-  let datDB = JSON.parse(localStorage.getItem('localDB')) || []; 
-  const allTasksContainer = document.querySelector('.task-items__all')
-  const inProgressTasksContainer = document.querySelector('.task-items__progress')
-  const finishedTasksContainer = document.querySelector('.task-items__finished')
+  let dataDB = JSON.parse(localStorage.getItem('localDB')) || []; 
+  const allTasksContainer = document.querySelector('.task-items__all');
+  const inProgressTasksContainer = document.querySelector('.task-items__progress');
+  const finishedTasksContainer = document.querySelector('.task-items__finished');
   const taskContainers = [...(document.querySelectorAll('.task-items'))];
 
   taskContainers.forEach((container) => {
     console.log(container);
     container.innerHTML = '';
   })
+
+  let renderTasks = function (tasks) {
+    tasks.forEach((task) => {
+      const taskElement = document.createElement('div');
+      taskElement.classList.add('task-item');
+      taskElement.textContent = task.taskText;
+
+      if (task.state === 'all_tasks') {
+        allTasksContainer.appendChild(taskElement);
+      } else if (task.state === 'in_progress') {
+        inProgressTasksContainer.appendChild(taskElement);
+      } else if (task.state === 'finished') {
+        finishedTasksContainer.appendChild(taskElement);
+      }
+    })
+  }
+
+  console.log(dataDB)
+  renderTasks(dataDB);
 }
 
 export {displayTasks}
